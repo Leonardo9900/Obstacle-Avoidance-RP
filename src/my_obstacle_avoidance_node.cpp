0e67f66df0ec0857b2b078ae8b2887c1831feac5
Leonardo Mongelli <73338494+Leonardo9900@users.noreply.github.com>
2023-10-19 11:05:00 +0200

    Update laserScanCallBack

--- src/my_obstacle_avoidance_node.cpp
+++ src/my_obstacle_avoidance_node.cpp
@@ -1,22 +1,26 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 #include <iostream>
-using namespace std
+using namespace std;
 
 void laserScanCallback(const sensor_msgs::LaserScan& msg)
 {
-  cerr << "I heard: as minimum range" << msg.range_min << endl;
+  
+  const std::vector<float>& ranges = msg.ranges;
+  float min_distance = *std::min_element(ranges.begin(), ranges.end());
+  
+  cerr << "The minimum distance from the wall is " << min_distance << " meters" << endl;
 }
 
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "laser_listener");
+  ros::init(argc, argv, "my_obstacle_avoidance_node");
   
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("/base_scan", 10, laserScanCallback);
-
+  ros::Subscriber laser_sub = n.subscribe("/base_scan", 10, laserScanCallback);
+  
   ros::spin();
 
   return 0;
