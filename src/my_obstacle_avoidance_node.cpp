a79928f0476b1d51afa1cb4e92ddd489ee4c5e32
Leonardo Mongelli <73338494+Leonardo9900@users.noreply.github.com>
2023-10-19 15:11:02 +0200

    first velocityScanCallback implemented

--- src/my_obstacle_avoidance_node.cpp
+++ src/my_obstacle_avoidance_node.cpp
@@ -1,7 +1,12 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Twist.h"
 #include <iostream>
 using namespace std;
+
+float velocity_x = 0;
+float velocity_y = 0;
+float velocity_z = 0;
 
 void laserScanCallback(const sensor_msgs::LaserScan& msg)
 {
@@ -12,6 +17,16 @@ void laserScanCallback(const sensor_msgs::LaserScan& msg)
   cerr << "The minimum distance from the wall is " << min_distance << " meters" << endl;
 }
 
+void velocityScanCallback(const geometry_msgs::Twist& vel_msg)
+{
+
+  velocity_x = vel_msg.linear.x;
+  velocity_y = vel_msg.linear.y;
+  velocity_z = vel_msg.angular.z;
+
+  cerr << "Actual velocity x= " << velocity_x << " , y= " << velocity_y << " , z= " << velocity_z << endl;
+}
+
 int main(int argc, char **argv)
 {
 
@@ -20,7 +35,8 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   ros::Subscriber laser_sub = n.subscribe("/base_scan", 10, laserScanCallback);
-  
+  ros::Subscriber vel_sub = n.subscribe("/cmd_vel", 10, velocityScanCallback);
+
   ros::spin();
 
   return 0;
